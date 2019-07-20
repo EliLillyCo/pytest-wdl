6c38bca7747bf50ed623f6ea806aaec0811e5c8e
jdidion <github@didion.net>
2019-07-20 09:49:59 -0400

    Make expected outputs optional; have test runner return outputs

---
+++ Makefile
@@ -0,0 +1,64 @@
+repo = EliLillyCo/LRL_cromwell_test_runner
+package = pytest_cromwell
+version = 0.1.1
+tests = tests
+
+BUILD = python setup.py bdist_wheel && pip install --upgrade dist/$(package)-$(version)-py3-none-any.whl $(installargs)
+TEST = pytest $(pytestops) $(tests)
+
+all:
+	$(BUILD)
+	$(TEST)
+
+install:
+	$(BUILD)
+
+test:
+	$(TEST)
+
+lint:
+	flake8 $(package)
+
+reformat:
+	black $(package)
+	black $(tests)
+
+clean:
+	rm -Rf __pycache__
+	rm -Rf **/__pycache__/*
+	rm -Rf **/*.c
+	rm -Rf **/*.so
+	rm -Rf **/*.pyc
+	rm -Rf dist
+	rm -Rf build
+	rm -Rf $(package).egg-info
+
+docker:
+	# build
+	docker build -f Dockerfile -t $(repo):$(version) .
+	# add alternate tags
+	docker tag $(repo):$(version) $(repo):latest
+	# push to Docker Hub
+	docker login -u jdidion && \
+	docker push $(repo)
+release:
+
+	$(clean)
+	# tag
+	git tag $(version)
+	# build
+	$(BUILD)
+	$(TEST)
+	python setup.py sdist bdist_wheel
+	# release
+	python setup.py sdist upload -r omics-pypi
+	git push origin --tags
+	$(github_release)
+	#$(docker)
+
+github_release:
+	curl -v -i -X POST \
+		-H "Content-Type:application/json" \
+		-H "Authorization: token $(token)" \
+		https://api.github.com/repos/$(repo)/releases \
+		-d '{"tag_name":"$(version)","target_commitish": "master","name": "$(version)","body": "$(desc)","draft": false,"prerelease": false}'
--- pytest_cromwell/core.py
+++ pytest_cromwell/core.py
@@ -259,8 +259,8 @@ class CromwellHarness:
             self(*args, **kwargs, execution_dir=tmpdir)
 
     def run_workflow(
-        self, wdl_script, workflow_name, inputs, expected, **kwargs
-    ):
+        self, wdl_script, workflow_name, inputs, expected=None, **kwargs
+    ) -> dict:
         """
         Run a WDL workflow on given inputs, and check that the output matches
         given expected values.
@@ -279,6 +279,12 @@ class CromwellHarness:
                     written to this file only if it doesn't exist.
                 * java_args: Additional arguments to pass to Java runtime.
                 * cromwell_args: Additional arguments to pass to `cromwell run`.
+
+        Returns:
+            Dict of outputs.
+
+        Raises:
+            Exception if there was an error executing Cromwell.
         """
         if "execution_dir" in kwargs:
             LOG.warning(
@@ -364,14 +370,17 @@ class CromwellHarness:
 
         outputs = self.get_cromwell_outputs(exe.out)
 
-        for name, expected_value in expected.items():
-            key = f"{workflow_name}.{name}"
-            if key not in outputs:
-                raise AssertionError(f"Workflow did not generate output {key}")
-            if isinstance(expected_value, DataFile):
-                expected_value.assert_contents_equal(outputs[key])
-            else:
-                assert expected_value == outputs[key]
+        if expected:
+            for name, expected_value in expected.items():
+                key = f"{workflow_name}.{name}"
+                if key not in outputs:
+                    raise AssertionError(f"Workflow did not generate output {key}")
+                if isinstance(expected_value, DataFile):
+                    expected_value.assert_contents_equal(outputs[key])
+                else:
+                    assert expected_value == outputs[key]
+
+        return outputs
 
     def _get_path(self, path, check_exists=False):
         if not os.path.isabs(path):
