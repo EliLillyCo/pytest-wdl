6f43d4838d6f93f5eebe68439ad70e65225f62b2
Michael Neylon <neylon_michael_t@lilly.com>
2019-08-22 14:09:50 -0400

    apply license notice

--- pytest_wdl/__init__.py
+++ pytest_wdl/__init__.py
@@ -1,3 +1,17 @@
+#    Copyright 2019 Eli Lilly and Company
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
 """
 Fixtures for writing tests that execute WDL workflows using Cromwell. For testability
 purposes, the implementaiton of these fixtures is done in the pytest_wdl.fixtures
