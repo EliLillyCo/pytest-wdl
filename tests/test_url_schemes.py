1da840b9399c23a2814d3d724ea6bde2cbca7e42
jdidion <github@didion.net>
2019-10-07 12:51:45 -0400

    Add test for dx url handler

--- tests/test_url_schemes.py
+++ tests/test_url_schemes.py
@@ -1,7 +1,7 @@
 import logging
-from typing import Sequence, Optional
+from typing import Sequence, Optional, cast
 import urllib.request
-from urllib.request import Request
+from urllib.request import Request, urlopen
 
 import pytest
 
@@ -17,11 +17,14 @@ try:
     assert dxpy.SECURITY_CONTEXT
     no_dx = False
 except:
-    LOG.warning(
-        "dxpy is not installed or user is not logged into a DNAnexus account; "
-        "DNAnexus URL handler will not be tested"
-    )
     no_dx = True
+
+
+DX_SKIP_MSG = \
+    "dxpy is not installed or user is not logged into a DNAnexus account; " \
+    "DNAnexus URL handler will not be tested"
+DX_FILE_ID = "file-BgY4VzQ0bvyg22pfZQpXfzgK"
+DX_PROJECT_ID = "project-BQbJpBj0bvygyQxgQ1800Jkk"
 
 
 class MockResponse(BaseResponse):
@@ -86,6 +89,37 @@ def test_url_schemes():
         urllib.request._opener = opener
 
 
-@pytest.mark.skipif(no_dx)
+@pytest.mark.skipif(no_dx, reason=DX_SKIP_MSG)
 def test_dx():
-    pass
+    rsp = urlopen(f"dx://{DX_FILE_ID}")
+    assert isinstance(rsp, Response)
+    import pytest_wdl.url_schemes.dx
+    assert isinstance(rsp, pytest_wdl.url_schemes.dx.DxResponse)
+    with tempdir() as d:
+        outfile = d / "readme.txt"
+        cast(Response, rsp).download_file(outfile, False)
+        assert outfile.exists()
+        with open(outfile, "rt") as inp:
+            txt = inp.read()
+        assert txt.startswith("README.1st.txt")
+        assert txt.rstrip().endswith(
+            "SRR100022: Full exome to use as input to your analyses."
+        )
+
+
+@pytest.mark.skipif(no_dx, reason=DX_SKIP_MSG)
+def test_dx_with_project():
+    rsp = urlopen(f"dx://{DX_PROJECT_ID}:{DX_FILE_ID}")
+    assert isinstance(rsp, Response)
+    import pytest_wdl.url_schemes.dx
+    assert isinstance(rsp, pytest_wdl.url_schemes.dx.DxResponse)
+    with tempdir() as d:
+        outfile = d / "readme.txt"
+        cast(Response, rsp).download_file(outfile, False)
+        assert outfile.exists()
+        with open(outfile, "rt") as inp:
+            txt = inp.read()
+        assert txt.startswith("README.1st.txt")
+        assert txt.rstrip().endswith(
+            "SRR100022: Full exome to use as input to your analyses."
+        )
